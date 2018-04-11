3cbdbc37758eea544d555fe686127da65b7d229e
Brandon Marcum <brandon.marcum9@gmail.com>
2018-04-11 19:24:47 -0400

    The last game, chicken fight is now ready.

---
+++ Casino.Library/Games/ChickenFight/Chicken.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casino.Library.Games.ChickenFight
+{
+    class Chicken
+    {
+        public int Health;
+        public int Attack;
+        public int Evasion;
+        public int StatTotal;
+        public bool Betted;
+        public bool Standing;
+
+        public Chicken()
+        {
+            Random rand = new Random();
+
+            StatTotal = 400;
+            int x = StatTotal / int.Parse(1.6.ToString());
+            Health = rand.Next(5, x);
+            int j = (StatTotal - x) / int.Parse(2.ToString());
+            Attack = rand.Next(5, j);
+            Evasion = StatTotal - (Health + Attack);
+
+            Standing = true;
+            Betted = false;
+        }
+
+        public int DecreaseHealth(int damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0)
+                Standing = false;
+
+            return Health;
+        }
+
+        public bool Evade()
+        {
+            Random rand = new Random();
+
+            if (Evasion < rand.Next(StatTotal * int.Parse(1.5.ToString())))
+                return false;
+            
+            return true;
+        }
+
+    }
+}
---
+++ Casino.Library/Games/ChickenFight/Fight.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casino.Library.Games.ChickenFight
+{
+    class Fight
+    {
+        Chicken chickenA;
+        Chicken chickenB;
+
+        public Fight()
+        {
+            chickenA = new Chicken();
+            chickenB = new Chicken();
+        }
+
+        public void PlaceBetA()
+        {
+            chickenA.Betted = true;
+        }
+
+        public void PlaceBetB()
+        {
+            chickenB.Betted = true;
+        }
+
+        public string Engage()
+        {
+
+            while (chickenA.Standing && chickenB.Standing)
+            {
+                if(chickenA.Betted)
+                {
+                    ChickenBTurn();
+                    ChickenATurn();
+                }
+                else if (chickenB.Betted)
+                {
+                    ChickenATurn();
+                    ChickenATurn();
+                }
+            }
+
+            return CheckWin();
+        }
+
+        private string CheckWin()
+        {
+            if (chickenA.Betted && !chickenA.Standing)
+                return "lose";
+            else if (chickenB.Betted && !chickenB.Standing)
+                return "lose";
+            else
+                return "win";
+        }
+
+        private void ChickenATurn()
+        {
+            if (!chickenB.Evade() && chickenA.Standing)
+                chickenB.DecreaseHealth(chickenA.Attack);
+        }
+
+        private void ChickenBTurn()
+        {
+            if (!chickenA.Evade())
+                chickenA.DecreaseHealth(chickenB.Attack);
+        }
+    }
+}
