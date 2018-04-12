f5f4812923e6697184965e07d9573aaf5c27100e
Jermaine Oliver <jermaine.r.oliver@wmich.edu>
2018-04-12 14:55:45 -0400

    fixed chip helper and pocket

--- Casino.Client/Controllers/GameController.cs
+++ Casino.Client/Controllers/GameController.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Casino.Client.Models;
 using Casino.Library.Games;
-//my change hello
+using Casino.Library.Models;
+
 namespace Casino.Client.Controllers
 {
     public class GameController : Controller
@@ -23,6 +24,9 @@ namespace Casino.Client.Controllers
         [HttpPost]
         public IActionResult BlackJack(BlackJackViewModel model, string submitButton)
         {
+            User newUser = new User();
+            
+
             model = TempData.Get<BlackJackViewModel>("model");
             
             if(submitButton.Equals("hit"))
@@ -37,12 +41,50 @@ namespace Casino.Client.Controllers
                 model.Blackjack.PlayerStand();
                 model.Blackjack.NextTurn();
                 ViewData["game"] = model.Blackjack.status;
+                ViewData["score"] = model.Blackjack.playerTotal;
+                ViewData["dealer"] = model.Blackjack.dealerTotal;
             }
             if(submitButton.Equals("play"))
             {
                 model.Blackjack = new Blackjack();
             }
             TempData.Put("model", model);
+
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Slots()
+        {
+            return View(new SlotsViewModel());
+        }
+        [HttpPost]
+        public IActionResult Slots(SlotsViewModel model, string submitButton)
+        {
+            try
+            {
+            model = TempData.Get<SlotsViewModel>("slots");
+            string k = model.Slots.status;
+            }
+            catch
+            {
+                model = new SlotsViewModel();
+            }
+
+            ViewData["left"] = model.Slots.left;
+            ViewData["middle"] = model.Slots.middle;
+            ViewData["right"] = model.Slots.right;
+
+            if(submitButton.Equals("run"))
+            {
+                model.Slots.SetSlots();
+            }
+            if(submitButton.Equals("stop"))
+            {
+                model.Slots.StopPlaying();
+            }
+            ViewData["status"] = model.Slots.status;
+
+            TempData.Put("slots", model);
 
             return View(model);
         }
